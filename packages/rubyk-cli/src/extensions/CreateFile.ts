1a5f415f86a67e442edeed42b7850973418b0ac9
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-01-21 02:01:31 -0300

    fix: found templates

--- packages/rubyk-cli/src/extensions/CreateFile.ts
+++ packages/rubyk-cli/src/extensions/CreateFile.ts
@@ -1,10 +1,14 @@
 import { GluegunToolbox } from "gluegun";
 
 module.exports = (toolbox: GluegunToolbox) => {
-  const { template, print: { success } } = toolbox
+  const { template, filesystem, print: { success } } = toolbox
 
   async function createFile(folder: string, fileName: string, content: string, contentProps?: {[x: string]: any}) {
     await template.generate({
+      directory: filesystem.path(
+        filesystem.cwd(), 
+        'node_modules/@rubykgen/rubyk-cli/build/templates/src/templates', 
+      ),
       template: content,
       target: `${folder}/${fileName}`,
       props: contentProps || {}
